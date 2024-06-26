10b2b1a47e1d8a89675200dc0df098b5ee77a52f
mrhimanshu700 <156831028+mrhimanshu700@users.noreply.github.com>
2024-06-26 06:14:33 +0530

    Update vite.config.js

--- chat_app/vite.config.js
+++ chat_app/vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':'https://chat-app-qxlf.onrender.com',
-    }
-  },
+  // server:{
+  //   proxy:{
+  //     '/api':'https://chat-app-qxlf.onrender.com',
+  //   }
+  // },
   plugins: [react()],
 })
