e07457484a6604d9c528645a26d41d3ed87258d2
mrhimanshu700 <156831028+mrhimanshu700@users.noreply.github.com>
2024-06-26 07:42:49 +0530

    Update SignUp.jsx

--- chat_app/src/component/authentication/SignUp.jsx
+++ chat_app/src/component/authentication/SignUp.jsx
@@ -43,7 +43,7 @@ function SignUp() {
         },
       };
       const { data } = await axios.post(
-        "https://chat-app-qxlf.onrender.com/user",
+        "https://chat-app-qxlf.onrender.com/api/user",
         {
           username: Username,
           email,
