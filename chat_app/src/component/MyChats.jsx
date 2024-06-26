dff70011d03ec900ecf63ec5b6f28c5613c79946
mrhimanshu700 <156831028+mrhimanshu700@users.noreply.github.com>
2024-06-26 06:31:55 +0530

    Update MyChats.jsx

--- chat_app/src/component/MyChats.jsx
+++ chat_app/src/component/MyChats.jsx
@@ -22,7 +22,7 @@ function MyChats({ fetchAgain }) {
         },
       };
 
-      const { data } = await axios.get("/api/chat", config);
+      const { data } = await axios.get("https://chat-app-qxlf.onrender.com/chat", config);
       console.log(data);
       setChats(data);
     } catch (error) {
